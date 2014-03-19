29bd2c9d27ed259d2c2d20e298152792a22f42cc
Lasse Schuirmann <lasse@schuirmann.net>
2014-03-19 19:49:35 +0100

    Added specification and first version of stdmacros.h

---
+++ src/stdmacros.h
@@ -0,0 +1,29 @@
+// This file is part of CE3D. License: GPL3
+
+#ifndef _STDMACROS_H
+#define _STDMACROS_H
+
+#define UNUSED __attribute__((unused))
+
+#ifdef __cplusplus
+
+/**
+ * TODO doxygen comment
+ * TODO doxygen comments for getters and setters
+ */
+#define AUTO_PROPERTY(type, name, orig_scope) type m_ ## (name);               \
+                            public:                                            \
+                                    inline type Get ## (name)() const          \
+                                    {                                          \
+                                        return m_ ## (name);                   \
+                                    }                                          \
+                                    inline type Set ## (name)(const type copy) \
+                                    {                                          \
+                                        m_ ## (name) = copy;                   \
+                                    }                                          \
+                            orig_scope:
+
+#endif
+
+#endif /* _STDMACROS_H */
+
