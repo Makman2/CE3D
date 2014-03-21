c04396b1a6c6bd4fb97f6e981fe138ae384863c7
Lasse Schuirmann <lasse@schuirmann.net>
2014-03-21 18:33:14 +0100

    Added model objects.

---
+++ src/model/model.h
@@ -0,0 +1,22 @@
+// This file is part of CE3D. License: GPL3
+
+#ifndef _MODEL_H
+#define _MODEL_H
+
+#include <stdmacro.h>
+
+namespace CE3D
+{
+
+/**
+ * TODO
+ */
+class Model
+{
+public:
+private:
+}
+
+}
+
+#endif /* _MODEL_H */
---
+++ src/model/point_model.h
@@ -0,0 +1,24 @@
+// This file is part of CE3D. License: GPL3
+
+#ifndef _POINT_MODEL_H
+#define _POINT_MODEL_H
+
+#include <stdmacro.h>
+
+#include "model.h"
+
+namespace CE3D
+{
+
+/**
+ * TODO
+ */
+class PointModel : public Model
+{
+public:
+private:
+}
+
+}
+
+#endif /* _POINT_MODEL_H */
