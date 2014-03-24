4760858702998633f9d2d87b1837e09b6c949790
Makman2 <mak95@arcor.de>
2014-03-25 00:51:43 +0100

    Updated world.

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -1,3 +1,56 @@
 // This file is part of CE3D. License: GPL3
 
 #include "world.h"
+
+
+namespace CE3D
+{
+    boost::ptr_list<Model> World::Models()
+    {
+        return this->m_Models;
+    }
+
+    void World::Transform(const boost::numeric::ublas::matrix<ModelDataType> matrix)
+    {
+        for (auto it : this->Models())
+        {
+            it.Transform(matrix);
+        }
+    }
+
+    void World::Translate(
+        const boost::numeric::ublas::vector<ModelDataType> translation)
+    {
+        for (auto it : this->Models())
+        {
+            it.Translate(translation);
+        }
+    }
+
+    void World::Scale(const ModelDataType factor)
+    {
+        for (auto it : this->Models())
+        {
+            it.Scale(factor);
+        }
+    }
+
+    void World::Scale(const boost::numeric::ublas::vector<ModelDataType> scale)
+    {
+        for (auto it : this->Models())
+        {
+            it.Scale(scale);
+        }
+    }
+
+    void World::Rotate(const boost::numeric::ublas::vector<ModelDataType> planar1,
+        const boost::numeric::ublas::vector<ModelDataType> planar2,
+        const boost::numeric::ublas::vector<ModelDataType> offset,
+        const float angle)
+    {
+        for (auto it : this->Models())
+        {
+            it.Rotate(planar1, planar2, offset, angle);
+        }
+    }
+}
--- src/world/world.h
+++ src/world/world.h
@@ -18,8 +18,55 @@ namespace CE3D
 class World
 {
 public:
+    /**
+     * The models the world contains.
+     *
+     * @return The list of models.
+     */
+    boost::ptr_list<Model> Models();
+
+    /**
+     * Transforms all models in the world.
+     *
+     * @param matrix: The matrix to transform with.
+     */
+    void Transform(const boost::numeric::ublas::matrix<ModelDataType> matrix);
+    /**
+     * Translates all models in the world.
+     *
+     * @param translation: The vector that describes the translation.
+     */
+    void Translate(const boost::numeric::ublas::vector<ModelDataType> translation);
+    /**
+     * Scales all models in the world.
+     *
+     * @param factor: The factor to scale with.
+     */
+    void Scale(const ModelDataType factor);
+    /**
+     * Scales all models in the world.
+     *
+     * @param scale: The vector that describes the scale.
+     */
+    void Scale(const boost::numeric::ublas::vector<ModelDataType> scale);
+    /**
+     * Rotates all models in the world.
+     *
+     * @param planar1: The first vector of the rotation plane.
+     * @param planar2: The second vector of the rotation plane.
+     * @param offset: The offset of the rotation axis.
+     * @param angle: The rotation angle.
+     */
+    void Rotate(const boost::numeric::ublas::vector<ModelDataType> planar1,
+        const boost::numeric::ublas::vector<ModelDataType> planar2,
+        const boost::numeric::ublas::vector<ModelDataType> offset,
+        const float angle);
+
+
+protected:
+
 private:
-    PROPERTY(private, boost::ptr_list<Model>, ModelList);
+    boost::ptr_list<Model> m_Models;
 };
 
 }
