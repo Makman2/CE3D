5ac0b8cf06d1c208b6e0290c36cf73577a02d33e
Lasse Schuirmann <lasse@schuirmann.net>
2014-03-24 06:48:32 +0100

    Add comments for stdmacro.h.

--- src/util/stdmacro.h
+++ src/util/stdmacro.h
@@ -12,18 +12,33 @@
 #define UNUSED __attribute__((unused))
 
 /**
- * TODO doxygen comment
- * TODO doxygen comments for getters and setters
+ * This macro creates a property with a public getter and setter.
+ *
+ * @param scope The scope of the property [private, public] this scope will be
+ * also active after this declaration.
+ * @param type The type of this property.
+ * @param name a name. The property will be privately accessible by m_ ## name.
  */
 #define RW_PROPERTY(scope, type, name);                                        \
 scope:                                                                         \
     type m_ ## name;                                                           \
 public:                                                                        \
+    /**                                                                        \
+     * Generic getter for properties.                                          \
+     *                                                                         \
+     * @return the property value.                                             \
+     */                                                                        \
     inline type Get ## name() const                                            \
     {                                                                          \
         return m_ ## name;                                                     \
     }                                                                          \
                                                                                \
+    /**                                                                        \
+     * Generic setter for properties.                                          \
+     *                                                                         \
+     * Please note that the position of the const is necessary for pointers.   \
+     * @param the property value.                                              \
+     */                                                                        \
     inline void Set ## name(type const copy)                                   \
     {                                                                          \
         m_ ## name = copy;                                                     \
@@ -31,13 +46,22 @@ public:                                                                        \
 scope:
 
 /**
- * TODO doxygen comment
- * TODO doxygen comments for getters and setters
+ * This macro creates a property with a public getter.
+ *
+ * @param scope The scope of the property [private, public] this scope will be
+ * also active after this declaration.
+ * @param type The type of this property.
+ * @param name a name. The property will be privately accessible by m_ ## name.
  */
 #define R_PROPERTY(scope, type, name);                                         \
 scope:                                                                         \
     type m_ ## name;                                                           \
 public:                                                                        \
+    /**                                                                        \
+     * Generic getter for properties.                                          \
+     *                                                                         \
+     * @return the property value.                                             \
+     */                                                                        \
     inline type Get ## name() const                                            \
     {                                                                          \
         return m_ ## name;                                                     \
@@ -45,43 +69,49 @@ public:                                                                        \
 scope:
 
 /**
- * TODO doxygen comment
- * TODO doxygen comments for getters and setters
+ * This macro creates a property with a public setter.
+ *
+ * @param scope The scope of the property [private, public] this scope will be
+ * also active after this declaration.
+ * @param type The type of this property.
+ * @param name a name. The property will be privately accessible by m_ ## name.
  */
 #define W_PROPERTY(scope, type, name);                                         \
 scope:                                                                         \
     type m_ ## name;                                                           \
 public:                                                                        \
+    /**                                                                        \
+     * Generic setter for properties.                                          \
+     *                                                                         \
+     * Please note that the position of the const is necessary for pointers.   \
+     * @param the property value.                                              \
+     */                                                                        \
     inline void Set ## name(type const copy)                                   \
     {                                                                          \
         m_ ## name = copy;                                                     \
     }                                                                          \
 scope:
 
+
 /**
- * TODO doxygen comment
- * TODO doxygen comments for getters and setters
+ * This macro creates a property.
+ *
+ * @param scope The scope of the property [private, public] this scope will be
+ * also active after this declaration.
+ * @param type The type of this property.
+ * @param name a name. The property will be privately accessible by m_ ## name.
  */
 #define PROPERTY(scope, type, name);                                           \
 scope:                                                                         \
     type m_ ## name
 
 /**
-* TODO doxygen comment
-*/
+ * Data type for model vectors.
+ */
 typedef float ModelDataType;
 /**
-* TODO doxygen comment
-*/
+ * Index type for model vectors.
+ */
 typedef uint32_t ModelIdxType;
-
-/**
-* TODO doxygen comment
-*/
-typedef float model_data_t;
-/**
-* TODO doxygen comment
-*/
-typedef uint32_t model_idx_t;
 
 #endif /* _STDMACRO_H */
