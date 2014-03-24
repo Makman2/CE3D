3624fae20b5a4a79ee34bf7f21385fd8ba65f7e9
Makman2 <mak95@arcor.de>
2014-03-24 23:23:51 +0100

    Merged model branch.

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -5,15 +5,153 @@
 #include <boost/numeric/ublas/operation_blocked.hpp>
 #include <boost/numeric/ublas/operation.hpp>
 
+
 namespace CE3D
 {
 
-void Model::Transform(boost::numeric::ublas::matrix<ModelDataType> matrix)
-{
-    for (auto& it : m_Vectors)
+    std::string Model::GetName()
     {
-        boost::numeric::ublas::axpy_prod(matrix, it, it, true);
+        return this->m_Name;
     }
-}
+    void Model::SetName(std::string value)
+    {
+        this->m_Name = value;
+    }
+
+    bool Model::IsVisible()
+    {
+        return this->m_IsVisible;
+    }
+    void Model::SetVisibility(bool value)
+    {
+        this->m_IsVisible = value;
+    }
+
+
+    void Model::Transform(boost::numeric::ublas::matrix<ModelDataType> matrix)
+    {
+        for (auto it : this->Vectors())
+        {
+            boost::numeric::ublas::axpy_prod(matrix, it, it, true);
+        }	
+    }
+
+    void Model::Translate(boost::numeric::ublas::vector<ModelDataType> translation)
+    {
+        // It's more performant to directly add the vertices together instead of
+        // calling the Transform() function and creating a matrix for it.
+        for (auto it : this->Vectors())
+        {
+            it += translation;
+        }
+    }
+
+    void Model::Scale(const ModelDataType factor)
+    {
+        // And again it's more performant not to use matrices.
+        for (auto it : this->Vectors())
+        {
+            it *= factor;
+        }
+    }
+
+    void Model::Scale(const boost::numeric::ublas::vector<ModelDataType> scale)
+    {
+        for (auto it : this->Vectors())
+        {
+            it = boost::numeric::ublas::element_prod(it, scale);
+        }
+    }
+
+    void Model::Rotate(const boost::numeric::ublas::vector<ModelDataType> planar1,
+        const boost::numeric::ublas::vector<ModelDataType> planar2, 
+    const boost::numeric::ublas::vector<ModelDataType> offset, const float angle)
+    {
+        this->Transform(CreateRotation(planar1, planar2, offset, angle));
+    }
+
+    boost::numeric::ublas::matrix<ModelDataType> Model::CreateTranslation(
+        boost::numeric::ublas::vector<ModelDataType> translation)
+    {
+        boost::numeric::ublas::matrix<ModelDataType> matrix(
+            translation.size() + 1, translation.size() + 1);
+        
+        for (boost::numeric::ublas::matrix<ModelDataType>::size_type row = 0;
+             row < matrix.size1(); row++)
+        {
+            boost::numeric::ublas::matrix<ModelDataType>::size_type column;
+             
+            for (column = 0; column < matrix.size2() - 1; column++)
+            {
+                if (row == column)
+                    matrix(row, column) = 1.0f;
+                else
+                    matrix(row, column) = 0.0f;
+            }
+            
+            column++;
+            if (row == matrix.size1() - 1)
+                matrix(row, column) = 1.0f;
+            else
+                matrix(row, column) = translation(row);
+        }
+
+        return matrix;
+    }
+
+
+    boost::numeric::ublas::matrix<ModelDataType> Model::CreateScale(
+        const boost::numeric::ublas::vector<ModelDataType> scale)
+    {
+        boost::numeric::ublas::matrix<ModelDataType> matrix(
+            scale.size(), scale.size());            
+        
+        for (boost::numeric::ublas::matrix<ModelDataType>::size_type row = 0;
+             row < matrix.size1(); row++)
+        {
+            for (boost::numeric::ublas::matrix<ModelDataType>::size_type column = 0;
+                column < matrix.size2(); column++)
+            {
+                if (row == column)
+                    matrix(row, column) = scale(row);
+                else
+                    matrix(row, column) = 0.0f;
+            }
+        }
+
+        return matrix;
+    }
+
+    boost::numeric::ublas::matrix<ModelDataType> Model::CreateScale(
+    const ModelDataType factor,
+    const boost::numeric::ublas::vector<ModelDataType>::size_type dimension)
+    {
+        boost::numeric::ublas::matrix<ModelDataType> matrix(dimension, dimension);            
+        
+        for (boost::numeric::ublas::matrix<ModelDataType>::size_type row = 0;
+             row < matrix.size1(); row++)
+        {
+            for (boost::numeric::ublas::matrix<ModelDataType>::size_type column = 0;
+                column < matrix.size2(); column++)
+            {
+                if (row == column)
+                    matrix(row, column) = factor;
+                else
+                    matrix(row, column) = 0.0f;
+            }
+        }
+
+        return matrix;
+    }
+
+
+
+    boost::numeric::ublas::matrix<ModelDataType> Model::CreateRotation(
+        const boost::numeric::ublas::vector<ModelDataType> planar1,
+        const boost::numeric::ublas::vector<ModelDataType> planar2,
+        const boost::numeric::ublas::vector<ModelDataType> offset, const float angle)
+    {
+        // TODO Implement Model::CreateRotation.
+    }
 
 }
--- src/model/model.h
+++ src/model/model.h
@@ -19,34 +19,127 @@ namespace CE3D
 class Model
 {
 public:
-        // TODO: Doxygen comments for all members and functions
-        bool IsVisible();
-        bool SetVisibility();
-        // I'm not convinced of this method... what does it do?
-        /*boost::container::vector<
-            boost::numeric::ublas::vector<ModelDataType>
-            > Vectors();*/
+    
+    /**
+     * Determines whether the model should be rendered or not.
+     *
+     * @return true if visible, false if not.
+     */
+    bool IsVisible();
+    /**
+     * Sets whether the model should be rendered or not.
+     *
+     * @param value: The visibility state. true for visible, false for hidden.
+     */
+    void SetVisibility(bool value);
+    /**
+     * Gets the list of vectors the model contains.
+     * 
+     * @return The list of vectors.
+     */
+    boost::container::vector<boost::numeric::ublas::vector<ModelDataType>> Vectors();
 
-        void Transform(boost::numeric::ublas::matrix<ModelDataType> matrix);
-        void Translate(boost::numeric::ublas::vector<ModelDataType>
-                           translation);
-        void Scale(ModelDataType factor);
-        void Scale(boost::numeric::ublas::vector<ModelDataType> scale);
-        void Rotate(boost::numeric::ublas::vector<ModelDataType> axis,
-                    boost::numeric::ublas::vector<ModelDataType> offset,
-                    float angle);
-        void RotateX(float angle);
-        void RotateY(float angle);
-        void RotateZ(float angle);
+    /**
+     * Gets the name or ID of the model.
+     * 
+     * @return The name.
+     */
+    std::string GetName();
+    /**
+     * Sets the name or ID of the model.
+     * 
+     * @param value: The new name.
+     */
+    void SetName(std::string value);
+
+    /**
+     * Transforms all vertices in the model.
+     *
+     * @param matrix: The matrix to transform with.
+     */
+    void Transform(const boost::numeric::ublas::matrix<ModelDataType> matrix);
+    /**
+     * Translates all vertices in the model.
+     *
+     * @param translation: The vector that describes the translation.
+     */
+    void Translate(const boost::numeric::ublas::vector<ModelDataType> translation);
+    /**
+     * Scales all vertices in the model.
+     *
+     * @param factor: The factor to scale with.
+     */
+    void Scale(const ModelDataType factor);
+    /**
+     * Scales all vertices in the model.
+     *
+     * @param scale: The vector that describes the scale.
+     */
+    void Scale(const boost::numeric::ublas::vector<ModelDataType> scale);
+    /**
+     * Rotates all vertices in the model.
+     *
+     * @param planar1: The first vector of the rotation plane.
+     * @param planar2: The second vector of the rotation plane.
+     * @param offset: The offset of the rotation axis.
+     * @param angle: The rotation angle.
+     */
+    void Rotate(const boost::numeric::ublas::vector<ModelDataType> planar1,
+        const boost::numeric::ublas::vector<ModelDataType> planar2,
+        const boost::numeric::ublas::vector<ModelDataType> offset, const float angle);
+
+
+
+    /**
+     * Creates a translation matrix.
+     *
+     * @param translation: The translation vector."
+     * @return A matrix that describes a translation.
+     */
+    static boost::numeric::ublas::matrix<ModelDataType> CreateTranslation(
+        boost::numeric::ublas::vector<ModelDataType> translation);
+
+    /**
+     * Creates a scale matrix.
+     *
+     * @param scale: The vector that describes the desired scale.
+     * @return A matrix that describes a scale.
+     */
+    static boost::numeric::ublas::matrix<ModelDataType> CreateScale(
+        const boost::numeric::ublas::vector<ModelDataType> scale);
+
+    /**
+     * Creates a scale matrix.
+     *
+     * @param factor: The factor to scale with.
+     * @param dimension: The dimension of the matrix.
+     * @return A matrix that describes a scale.
+     */
+    static boost::numeric::ublas::matrix<ModelDataType> CreateScale(
+        const ModelDataType factor,
+        const boost::numeric::ublas::vector<ModelDataType>::size_type dimension);
+    
+    /**
+     * Creates a rotation matrix.
+     *
+     * @param planar1: The first vector of the rotation plane.
+     * @param planar2: The second vector of the rotation plane.
+     * @param ofset: The rotation offset.
+     * @param angle: The rotation angle.
+     */
+    static boost::numeric::ublas::matrix<ModelDataType> CreateRotation(
+        const boost::numeric::ublas::vector<ModelDataType> planar1,
+        const boost::numeric::ublas::vector<ModelDataType> planar2,
+        const boost::numeric::ublas::vector<ModelDataType> offset, const float angle);
+
 protected:
-        R_PROPERTY(protected,
-            boost::container::vector<
-                boost::numeric::ublas::vector<ModelDataType>
-                >,
-            Vectors);
+    
+    
 private:
-        PROPERTY(private, bool, IsVisible);
-        RW_PROPERTY(private, std::string, Name);
+    boost::container::vector<boost::numeric::ublas::vector<ModelDataType>> m_Vectors;
+    bool m_IsVisible;
+    std::string m_Name;
+
 };
 
 }
