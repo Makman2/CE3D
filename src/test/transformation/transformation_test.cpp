8ea6c7a805bdf67b8df3a4d000846ce034c3c512
Lasse Schuirmann <lasse@schuirmann.net>
2014-03-27 19:17:49 +0100

    Add 'test' directory to debug build

--- src/test/transformation/transformation_test.cpp
+++ src/test/transformation/transformation_test.cpp
@@ -1,9 +1,3 @@
 // This file is part of CE3D. License: GPL3
 
 #include "transformation_test.h"
-
-void CE3DTestTransformation(CE3D::Transformation::Transformation& Tester)
-{
-    // There's nothing to test yet.
-    return;
-}
