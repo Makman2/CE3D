c468f1e63cf503be5014a2f46d13b17f57e10d04
Lasse Schuirmann <lasse@schuirmann.net>
2014-03-27 06:47:11 +0100

    Corrected some include guards.

--- src/transformation/translation.h
+++ src/transformation/translation.h
@@ -1,7 +1,7 @@
 // This file is part of CE3D. License: GPL3
 
-#ifndef _TRANSLATION_H
-#define _TRANSLATION_H
+#ifndef CE3D_TRANSFORMATION_TRANSLATION_H
+#define CE3D_TRANSFORMATION_TRANSLATION_H
 
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/vector.hpp>
@@ -64,4 +64,4 @@ public:
 }
 }
 
-#endif /* _TRANSLATION_H */
+#endif /* CE3D_TRANSFORMATION_TRANSLATION_H */
