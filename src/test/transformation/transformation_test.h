60a33315425a903790e3b4dccadf04bac8863ca9
Lasse Schuirmann <lasse@schuirmann.net>
2014-03-28 16:02:26 +0100

    Minor changes.

--- src/test/transformation/transformation_test.h
+++ src/test/transformation/transformation_test.h
@@ -3,14 +3,12 @@
 #ifndef CE3D_TEST_TRANSFORMATION_TRANSFORMATION_TEST_H
 #define CE3D_TEST_TRANSFORMATION_TRANSFORMATION_TEST_H
 
-#include "transformation/transformation.h"
-
 /**
  * This test method ensures that the intended functionality of any derived
  * object provided by the Transformation base class is available.
  *
  * @param Tester The object under test.
  */
-void CE3DTestTransformation(CE3D::Transformation::Transformation& Tester);
+void CE3DTestTransformation();
 
 #endif /* CE3D_TEST_TRANSFORMATION_TRANSFORMATION_TEST_H */
