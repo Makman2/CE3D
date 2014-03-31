20fb25d50de5a0fce80c5c47c8543dc61680a793
Makman2 <mak95@arcor.de>
2014-03-31 22:26:34 +0200

    Fixed extensions file layout.

--- src/util/boost_extensions.h
+++ src/util/boost_extensions.h
@@ -3,14 +3,46 @@
 #ifndef CE3D_UTIL_BOOST_EXTENSIONS_H
 #define CE3D_UTIL_BOOST_EXTENSIONS_H
 
-#include "util/boost_extensions_code.h"
-
 namespace boost
 {
 namespace numeric
 {
 namespace ublas
 {
+
+/**
+ * Inverts a given matrix.
+ *
+ * @param input: The matrix to invert.
+ * @param inverse: A reference to a matrix where the result should be stored.
+ * @return true on success, false on fail
+ * (for example if the matrix is linearly dependent).
+ */
+template<typename T>
+bool
+invert(matrix<T> const& input, matrix<T>& inverse);
+
+
+/**
+ * Concats vectors together to a matrix.
+ *
+ * @param vectors: An array that contains the vectors that should be concatenated.
+ * @param count: The number of elements in the array.
+ * @return The concatenated matrix.
+ */
+template<typename T>
+matrix<T>
+concat_vectors(vector<T> const * const vectors, size_t count);
+
+/**
+ * Concats vectors together to a matrix.
+ *
+ * @param vectors: The std::vector list that contains the vectors to concat.
+ * @return The concatenated matrix.
+ */
+template<typename T>
+matrix<T>
+concat_vectors(std::vector<vector<T>> const& vectors);
 
 }
 }
--- src/util/boost_extensions_code.h
+++ src/util/boost_extensions_code.h
@@ -15,14 +15,6 @@ namespace numeric
 namespace ublas
 {
 
-/**
- * Inverts a given matrix.
- *
- * @param input: The matrix to invert.
- * @param inverse: A reference to a matrix where the result should be stored.
- * @return true on success, false on fail
- * (for example if the matrix is linearly dependent).
- */
 template<typename T>
 bool
 invert(matrix<T> const& input, matrix<T>& inverse)
@@ -47,13 +39,6 @@ invert(matrix<T> const& input, matrix<T>& inverse)
     return true;
 }
 
-/**
- * Concats vectors together to a matrix.
- *
- * @param vectors: An array that contains the vectors that should be concatenated.
- * @param count: The number of elements in the array.
- * @return The concatenated matrix.
- */
 template<typename T>
 matrix<T>
 concat_vectors(vector<T> const * const vectors, size_t count)
@@ -77,12 +62,6 @@ concat_vectors(vector<T> const * const vectors, size_t count)
     return M;
 }
 
-/**
- * Concats vectors together to a matrix.
- *
- * @param vectors: The std::vector list that contains the vectors to concat.
- * @return The concatenated matrix.
- */
 template<typename T>
 matrix<T>
 concat_vectors(std::vector<vector<T>> const& vectors)
