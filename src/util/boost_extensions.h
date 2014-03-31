a714af208cc95b76f63d15c9bf1fd8adb3cd17b5
Makman2 <mak95@arcor.de>
2014-03-31 16:06:04 +0200

    Updated orthogonal transformation and uploaded spec.

--- src/util/boost_extensions.h
+++ src/util/boost_extensions.h
@@ -12,9 +12,38 @@ namespace numeric
 namespace ublas
 {
 
+/**
+ * Inverts a given matrix.
+ *
+ * @param input: The matrix to invert.
+ * @param inverse: A reference to a matrix where the result should be stored.
+ * @return true on success, false on fail
+ * (for example if the matrix is linearly dependent).
+ */
 template<class T>
 bool
 invert(matrix<T> const& input, matrix<T>& inverse);
+
+/**
+ * Concats vectors together to a matrix.
+ *
+ * @param vectors: An array that contains the vectors that should be concatenated.
+ * @param count: The number of elements in the array.
+ * @return The concatenated matrix.
+ */
+template<class T>
+boost::numeric::ublas::matrix<T>
+concat_vectors(boost::numeric::ublas::vector<T> const * const vectors, size_t count);
+
+/**
+ * Concats vectors together to a matrix.
+ *
+ * @param vectors: The std::vector list that contains the vectors to concat.
+ * @return The concatenated matrix.
+ */
+template<class T>
+boost::numeric::ublas::matrix<T>
+concat_vectors(std::vector<boost::numeric::ublas::vector<T>> vectors);
 
 }
 }
