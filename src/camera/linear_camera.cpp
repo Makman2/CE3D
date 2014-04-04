f36cd45ad9c063cfd3c92fcb42602df5f1b01026
Lasse Schuirmann <lasse@schuirmann.net>
2014-04-04 16:42:28 +0200

    [camera] Add linear camera

---
+++ src/camera/linear_camera.cpp
@@ -0,0 +1,13 @@
+// This file is part of CE3D. License: GPL3
+
+#include "camera/linear_camera.h"
+
+namespace CE3D {
+
+LinearCamera::LinearCamera()
+{ }
+
+LinearCamera::~LinearCamera()
+{ }
+
+} /* namespace CE3D */
---
+++ src/camera/linear_camera.h
@@ -0,0 +1,20 @@
+// This file is part of CE3D. License: GPL3
+
+#ifndef CE3D_CAMERA_LINEAR_CAMERA_H
+#define CE3D_CAMERA_LINEAR_CAMERA_H
+
+#include "util/stdinc.h"
+
+#include "camera/camera.h"
+
+namespace CE3D {
+
+class LinearCamera : Camera {
+public:
+    LinearCamera();
+    virtual ~LinearCamera();
+};
+
+} /* namespace CE3D */
+
+#endif /* CE3D_CAMERA_LINEAR_CAMERA_H */
