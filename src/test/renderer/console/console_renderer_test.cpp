5ccc1b579e4bd401a3664b641fd429a0f55d591f
Lasse Schuirmann <lasse@schuirmann.net>
2014-04-18 15:18:45 +0200

    [test] Add ConsoleRendererTest

---
+++ src/test/renderer/console/console_renderer_test.cpp
@@ -0,0 +1,32 @@
+// This file is part of CE3D. License: GPL3
+
+#include "test/TestEnvironment.h"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Console
+#include <boost/test/unit_test.hpp>
+
+#include "util/stdinc.h"
+
+#include "renderer/console/console_renderer.h"
+
+namespace CE3D
+{
+namespace Testing
+{
+
+BOOST_FIXTURE_TEST_SUITE(ConsoleRenderer, TestEnvironment)
+
+/**
+ * Tests the construction and destruction of Console.
+ */
+BOOST_AUTO_TEST_CASE(TestConsoleRendererConstruction)
+{
+    // also tests the internal reference counting
+    CE3D::ConsoleRenderer renderer;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}
+}
