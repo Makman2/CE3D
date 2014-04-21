501d8a9477ea617b0d5df976e99643bb793cc556
Lasse Schuirmann <lasse@schuirmann.net>
2014-04-21 13:50:47 +0200

    [test] Move test from world to renderer

--- src/test/world/world_test.cpp
+++ src/test/world/world_test.cpp
@@ -10,9 +10,6 @@
 
 #include "world/world.h"
 #include "renderer/console/console_material.h"
-#include "model/model.h"
-#include "camera/linear_camera.h"
-#include "renderer/console/console_renderer.h"
 
 namespace CE3D
 {
@@ -27,47 +24,6 @@ BOOST_FIXTURE_TEST_SUITE(World, TestEnvironment)
 BOOST_AUTO_TEST_CASE(TestWorldConstruction)
 {
     CE3D::World<CE3D::ConsoleMaterial> TestWorld;
-}
-
-BOOST_AUTO_TEST_CASE(TestRenderer)
-{
-    // Create vector
-    Vector AVec(3);
-    // x position
-    AVec(0)=2;
-    // y position
-    AVec(1)=3;
-    // z position
-    AVec(2)=0;
-
-    // Create vertex
-    Vertex<CE3D::ConsoleMaterial> TestVertex;
-    // THIS DOESNT WORK YET
-    CE3D::ConsoleMaterial Mat(ConsoleColor::RED);
-    TestVertex.SetMaterial(Mat);
-    TestVertex.GetVectors().push_back(0);
-
-    // Create model
-    std::shared_ptr<CE3D::Model<CE3D::ConsoleMaterial> >
-    TestModel(new CE3D::Model<CE3D::ConsoleMaterial>());
-    TestModel->SetName("TestModel");
-    TestModel->GetVectors().push_back(AVec);
-    TestModel->GetVertices().push_back(TestVertex);
-
-    // Create world
-    std::shared_ptr<CE3D::World<CE3D::ConsoleMaterial> >
-    TestWorld(new CE3D::World<CE3D::ConsoleMaterial>);
-    TestWorld->AddModel(TestModel);
-
-    // Create camera
-    CE3D::LinearCamera<ConsoleMaterial> TestCamera;
-    TestCamera.SetWorld(TestWorld);
-
-    // Render it
-    CE3D::ConsoleRenderer Renderer;
-    Renderer.Render(TestCamera);
-
-    //while(1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
