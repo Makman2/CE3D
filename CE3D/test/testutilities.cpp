596588a942807e0b7376e96393a0ad4d0cfd218c
Makman2 <mak95@arcor.de>
2014-08-20 00:12:33 +0200

    test: Fix header inclusion
    testutilities.cpp itself points to the wrong
    header file.

--- CE3D/test/testutilities.cpp
+++ CE3D/test/testutilities.cpp
@@ -1,6 +1,6 @@
 // This file is part of CE3D. License: GPL3
 
-#include "CE3D/test/transformation/transformation_test.h"
+#include "CE3D/test/testutilities.h"
 
 #include <boost/test/unit_test.hpp>
 #include <stdlib.h>
